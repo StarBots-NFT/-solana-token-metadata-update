72ad041e238e8d0eb1529f354ce4f687c97dcb97
Nam Hoang <namhoang@starbots.net>
2021-12-10 14:58:15 +0700

    init

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from '@solana/web3.js';
+
+export const METAPLEX = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
+
+// this is Andy' devnet testing candy machine
+export const CANDY_MACHINE_ID = new PublicKey('2uaUddqUQvbkDkqTVQi7Zn4jFMArppgXwrA2iWBJZWKZ');
---
+++ src/index.ts
@@ -0,0 +1,165 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import bs58 from 'bs58';
+import { Keypair, PublicKey, Connection, clusterApiUrl, Cluster, Transaction } from '@solana/web3.js';
+import { getMultipleAccounts, sendSignedTransaction, loadMetadata, readPrivateKey } from './utils';
+import { Data } from './metaplex/classes';
+import { getMetadataAddress } from './metaplex/utils';
+import { createMetadata, updateMetadataInstruction } from './metaplex/metadata';
+
+const RPC_CLUSTER_SERUM = 'https://solana-api.projectserum.com';
+// const RPC_CLUSTER_DEV = 'https://api.devnet.solana.com';
+const RPC_CLUSTER = RPC_CLUSTER_SERUM;
+
+const getConnection = (env: string) => {
+    const cluster = env === 'mainnet-beta' ? RPC_CLUSTER : clusterApiUrl(env as Cluster);
+    const connection = new Connection(cluster);
+    return connection;
+};
+
+program.version('0.0.1');
+
+program
+    .command('update')
+    .option('-e, --env <string>', 'Solana cluster env name. One of: mainnet-beta, testnet, devnet', 'devnet')
+    // .option('-k, --keypair <path>', 'Solana wallet location', '--keypair not provided')
+    .action(async (_directory, cmd) => {
+        try {
+            const { env } = cmd.opts();
+
+            const connection = getConnection(env);
+
+            const decoded = bs58.decode(readPrivateKey());
+
+            const walletKeyPair = Keypair.fromSecretKey(decoded);
+
+            const metaKey: PublicKey[] = await Promise.all(
+                cmd.args.map((key: string) => getMetadataAddress(new PublicKey(key))),
+            );
+
+            const rawMetas = await getMultipleAccounts(
+                connection,
+                metaKey.map((e: PublicKey) => e.toBase58()),
+                'finalized',
+            );
+
+            // if not found, add instruction create account
+            for (let index = 0; index < rawMetas.keys.length; index++) {
+                const instructions = [];
+
+                const metaKey = rawMetas.keys[index];
+                const metaAccount = rawMetas.array[index];
+                if (!metaAccount) {
+                    console.log('if not found, add instruction create account');
+                }
+
+                const mintKey = cmd.args[index];
+                console.log(`mintKey = ${mintKey}`);
+                console.log(`metaKey = ${metaKey}`);
+
+                const updatedData = new Data(loadMetadata());
+
+                instructions.push(
+                    await updateMetadataInstruction(
+                        updatedData,
+                        walletKeyPair.publicKey.toBase58(), // newUpdateAuthority
+                        true,
+                        mintKey,
+                        walletKeyPair.publicKey.toBase58(), // updateAuthority
+                        // metadataAccountStr,
+                    ),
+                );
+                // console.log('instruction', instruction);
+                const tx = new Transaction().add(...instructions);
+
+                tx.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
+                tx.feePayer = walletKeyPair.publicKey;
+
+                tx.sign(walletKeyPair, walletKeyPair);
+                // console.log('tx', tx);
+
+                const { txid, slot } = await sendSignedTransaction({
+                    connection,
+                    signedTransaction: tx,
+                });
+
+                console.log('✅ Tx was successful! ID: ', txid, slot);
+            }
+        } catch (error) {
+            console.warn(`🚫 failed to set with error:`, error.message);
+        }
+    });
+
+program
+    .command('set')
+    .option('-e, --env <string>', 'Solana cluster env name. One of: mainnet-beta, testnet, devnet', 'devnet')
+    // .option('-k, --keypair <path>', 'Solana wallet location', '--keypair not provided')
+    .action(async (_directory, cmd) => {
+        try {
+            const { env } = cmd.opts();
+
+            const connection = getConnection(env);
+
+            const decoded = bs58.decode(readPrivateKey());
+
+            const walletKeyPair = Keypair.fromSecretKey(decoded);
+
+            const metaKey: PublicKey[] = await Promise.all(
+                cmd.args.map((key: string) => getMetadataAddress(new PublicKey(key))),
+            );
+
+            const rawMetas = await getMultipleAccounts(
+                connection,
+                metaKey.map((e: PublicKey) => e.toBase58()),
+                'finalized',
+            );
+
+            // if not found, add instruction create account
+            for (let index = 0; index < rawMetas.keys.length; index++) {
+                const instructions = [];
+
+                const metaKey = rawMetas.keys[index];
+                const metaAccount = rawMetas.array[index];
+                if (!metaAccount) {
+                    console.log('if not found, add instruction create account');
+                }
+
+                const mintKey = cmd.args[index];
+                console.log(`mintKey = ${mintKey}`);
+                console.log(`metaKey = ${metaKey}`);
+
+                const updatedData = new Data(loadMetadata());
+
+                instructions.push(
+                    await createMetadata(
+                        updatedData,
+                        walletKeyPair.publicKey.toBase58(), // newUpdateAuthority
+                        mintKey,
+                        walletKeyPair.publicKey.toBase58(), // updateAuthority
+                        walletKeyPair.publicKey.toBase58(),
+                    ),
+                );
+
+                // console.log('instruction', instruction);
+                const tx = new Transaction().add(...instructions);
+
+                tx.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
+                tx.feePayer = walletKeyPair.publicKey;
+
+                tx.sign(walletKeyPair, walletKeyPair);
+                // console.log('tx', tx);
+
+                const { txid, slot } = await sendSignedTransaction({
+                    connection,
+                    signedTransaction: tx,
+                });
+
+                console.log('✅ Tx was successful! ID: ', txid, slot);
+            }
+        } catch (error) {
+            console.warn(`🚫 failed to set with error:`, error.message);
+        }
+    });
+
+program.parse(process.argv);
